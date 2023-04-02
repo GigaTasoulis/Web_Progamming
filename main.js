8c35e167e692a7c066b9132506670f6d617debe0
Anastasios <119429929+GigaTasoulis@users.noreply.github.com>
2023-04-02 19:09:45 +0300

    Perfectly working

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// function scrollSlowly(event){
+//   var scrollLink = document.getElementById("scroll-link");
+
+// scrollLink.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   window.scrollBy(0,100) 
+// });
+// }
+
+
+
